ab0d7d37654ae0025563f15561c1bac0b3c39700
ahronl <aharon.levi@amobee.com>
2018-04-02 15:18:39 +0300

    fwpd

--- foundations/clojure-foundations/project.clj
+++ foundations/clojure-foundations/project.clj
@@ -1,9 +1,10 @@
 (defproject clojure-foundations "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "FIXME: learn how to program with clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot clojure-foundations.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :resource-paths ["src/clojure_foundations/fwpd/resources"]
+  :profiles {:uberjar {:aot [clojure-foundations.core clojure-foundations.fwpd.core]}})
