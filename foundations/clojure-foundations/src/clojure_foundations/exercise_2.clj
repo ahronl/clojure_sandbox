78fa4918a7ea87aee97d27bc692d34777c802672
ahronl <aharon.levi@amobee.com>
2018-04-04 12:55:11 +0300

    add question number 3

--- foundations/clojure-foundations/src/clojure_foundations/exercise_2.clj
+++ foundations/clojure-foundations/src/clojure_foundations/exercise_2.clj
@@ -17,3 +17,17 @@
   ([f & fs]
   (fn [& args] 
   	(f (apply (apply new-comp fs) args)))))
+
+(defn new-assoc-in
+	;question #3
+	[m [k & ks] v]
+	(loop 
+		[map m
+		key k
+		keys ks 
+		val v]
+		(if (empty? keys)
+			(assoc map key val)
+			(assoc map key (new-assoc-in (get map key {}) keys val)))))
+
+
--- foundations/clojure-foundations/test/clojure_foundations/exercise_2_test.clj
+++ foundations/clojure-foundations/test/clojure_foundations/exercise_2_test.clj
@@ -12,3 +12,11 @@
 (deftest new-comp-test
 	(testing "new-comp")
 	(is (= 90 ((clojure-foundations.exercise-2/new-comp :harms :strength :attributes) my-character))))
+
+(def users 
+	[{:name "James" :age 26}  {:name "John" :age 43}])
+
+(deftest assoc-in-test
+	(testing "assoc-in-test")
+	(is (= [{:name "James", :age 26} {:name "John", :age 44}] (assoc-in users [1 :age] 44)))
+	(is (= [{:name "James", :age 26} {:name "John", :age 43} {:name "Jack", :age 19}] (clojure-foundations.exercise-2/new-assoc-in users [2] {:name "Jack" :age 19}))))
