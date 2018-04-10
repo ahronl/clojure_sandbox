9c91876565a7bd4fce0e475de55212298c3d50a2
ahronl <aharon.levi@amobee.com>
2018-04-10 18:34:06 +0300

    concurency ex 2

--- foundations/clojure-foundations/src/clojure_foundations/concurrency/core.clj
+++ foundations/clojure-foundations/src/clojure_foundations/concurrency/core.clj
@@ -5,6 +5,8 @@
 
 (def urls ["https://www.bing.com/search?q=" "https://www.bing.com/search?q="])
 
+(def urls-map {:bing "https://www.bing.com/search?q=" :google "https://www.bing.com/search?q="})
+
 (defn run-search
 	[url word]
 	(slurp (str word url)))
@@ -13,6 +15,12 @@
 	[word]
 	(map #(run-search word %) urls))
 
+(defn search-me-ex2
+	[word engine]
+	(map #(run-search word %) [(get urls-map engine)]))
+
 (defn -main
 	[]
-	(println (search-me-ex1 "clojure")))
+	(do 
+		(println (search-me-ex1 "clojure"))
+		(println (search-me-ex2 "clojure" :bing))))
