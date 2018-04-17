bf0fd1946909875e1ac8431d59b9ba9410f02e80
ahronl <aharon.levi@amobee.com>
2018-04-17 17:16:22 +0300

    execersie 4 question 2

--- foundations/clojure-foundations/src/clojure_foundations/exercise_4.clj
+++ foundations/clojure-foundations/src/clojure_foundations/exercise_4.clj
@@ -8,3 +8,18 @@
 	(do (dotimes [n 10]
 		(swap! my-atom-value #(inc %)))
 	@my-atom-value))
+
+(defn count-the-words [quote]
+	(frequencies (map #(clojure.string/lower-case %) (filter #(not (empty? %)) (clojure.string/split quote #"[ .;?!\-\n\r]")))))
+
+(def rq-url "https://www.braveclojure.com/random-quote")
+
+(def word-count-map (atom {}))
+
+(defn get-word-count-from-quotes [num]
+	(do 
+		(dosync 
+			(doseq [futures (map (fn [x] (future (swap! word-count-map (fn [wc] (merge-with + wc (count-the-words (slurp rq-url))))))) (range num))] 
+				((fn [f] @f) futures)))
+		@word-count-map))
+		
--- foundations/clojure-foundations/test/clojure_foundations/exercise_4_test.clj
+++ foundations/clojure-foundations/test/clojure_foundations/exercise_4_test.clj
@@ -5,3 +5,7 @@
 (deftest inc-my-atom-value_ten_times-test
   (testing "inc-my-atom-value_ten_times")
     (is (= 10 (clojure-foundations.exercise-4/inc-my-atom-value-ten-times))))
+
+(deftest get-word-count-from-quotes-test
+	(testing "get-word-count-from-quotes"
+		(println (clojure-foundations.exercise-4/get-word-count-from-quotes 5))))
