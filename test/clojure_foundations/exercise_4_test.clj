d4f803fdb49eadb81370c352ad54680ab0ed5df1
ahronl <aharon.levi@amobee.com>
2018-04-23 11:02:28 +0300

    remove ns from tests

--- test/clojure_foundations/exercise_4_test.clj
+++ test/clojure_foundations/exercise_4_test.clj
@@ -4,12 +4,12 @@
 
 (deftest inc-my-atom-value_ten_times-test
   (testing "inc-my-atom-value_ten_times")
-    (is (= 10 (clojure-foundations.exercise-4/inc-my-atom-value-ten-times))))
+    (is (= 10 (inc-my-atom-value-ten-times))))
 
 (deftest get-word-count-from-quotes-test
 	(testing "get-word-count-from-quotes"
-		(println (clojure-foundations.exercise-4/get-word-count-from-quotes 5))))
+		(println (get-word-count-from-quotes 5))))
 
 (deftest heal-test
 	(testing "heal"
-		(clojure-foundations.exercise-4/heal)))
+		(heal)))
