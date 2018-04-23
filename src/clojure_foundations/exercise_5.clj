aae76b17d5fb827812fa3af4db7c3e0ff57f8b2a
ahronl <aharon.levi@amobee.com>
2018-04-23 11:01:27 +0300

    ex 5 Q 1 & 2

--- src/clojure_foundations/exercise_5.clj
+++ src/clojure_foundations/exercise_5.clj
@@ -1,17 +1,33 @@
 (ns clojure-foundations.exercise-5
   (:gen-class))
 
+
+(defmulti full-moon-behavior-multimethod (fn [were-creature] (:were-type were-creature)))
+
+(defmethod full-moon-behavior-multimethod :wolf
+  [were-creature]
+  (str (:name were-creature) " will howl and murder"))
+
+(defmethod full-moon-behavior-multimethod :simmons
+  [were-creature]
+  (str (:name were-creature) " will encourage people and sweat to the oldies"))
+
+
+(defmethod full-moon-behavior-multimethod :dancer
+  [were-creature]
+  (str (:name were-creature) " will dance"))
+
 (defprotocol WereCreature
-	(full-moon-behavior [x]))
+	(full-moon-behavior-protocal [x]))
 
 (defrecord WereWolf [name title]
   WereCreature
-  (full-moon-behavior [x]
+  (full-moon-behavior-protocal [x]
     (str name " will howl and murder")))
 
 (defrecord Dancer [name title]
   WereCreature
-  (full-moon-behavior [x]
+  (full-moon-behavior-protocal [x]
     (str name " will dance")))
 
-(full-moon-behavior (map->WereWolf {:name "Lucian" :title "CEO of Melodrama"}))
+(full-moon-behavior-protocal (map->WereWolf {:name "Lucian" :title "CEO of Melodrama"}))
--- test/clojure_foundations/exercise_5_test.clj
+++ test/clojure_foundations/exercise_5_test.clj
@@ -2,8 +2,13 @@
   (:require [clojure.test :refer :all]
             [clojure-foundations.exercise-5 :refer :all]))
 
-(deftest full-moon-behavior-test
-  (testing "ifull-moon-behavior")
-    (is (= "Lucian will howl and murder" (full-moon-behavior (map->WereWolf {:name "Lucian" :title "CEO of Melodrama"}))))
-    (is (= "Lucian will howl and murder" (full-moon-behavior (map->WereWolf {:name "Lucian" :title "CEO of Melodrama"})))))
+(deftest full-moon-behavior-multimethod-test
+  (testing "full-moon-behavior polymorphism using multimethod")
+    (is (= "Rachel from next door will howl and murder" (full-moon-behavior-multimethod {:were-type :wolf :name "Rachel from next door"})))
+    (is (= "Andy the baker will encourage people and sweat to the oldies" (full-moon-behavior-multimethod {:name "Andy the baker" :were-type :simmons}))))
+
+(deftest full-moon-behavior-protocal-test
+  (testing "full-moon-behavior polymorphism using recored and protocal")
+    (is (= "Lucian will howl and murder" (full-moon-behavior-protocal (map->WereWolf {:name "Lucian" :title "CEO of Melodrama"}))))
+    (is (= "Lucian will dance" (full-moon-behavior-protocal (map->Dancer {:name "Lucian" :title "CEO of Melodrama"})))))
 
